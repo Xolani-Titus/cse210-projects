dbdc4961b966d691d935131f5750c66e6f3bf51f
Xolani-Titus <tit19002@byui.edu>
2024-04-25 06:40:51 +0200

    Completion of Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+    
+        Console.Write("What is your name? ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string firstName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        
+        
     }
 }
