8d58f0c75b0f044579f3fc1e4459620b6a265e13
Xolani-Titus <tit19002@byui.edu>
2024-04-26 07:07:40 +0200

    Completion of Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int number = int.Parse(percentage);
+
+        string letter = "";
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >=70)
+        {
+            letter = "C";
+        }
+        else if (number >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (number >=70)
+        {
+            Console.WriteLine("Congratulation you passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Unfortunately you did not pass!");
+        }
     }
 }
