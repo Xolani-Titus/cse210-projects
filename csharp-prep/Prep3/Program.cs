12a10379086324c076c7f6fe87cf3c8fc0e08295
Xolani-Titus <tit19002@byui.edu>
2024-04-27 18:29:25 +0200

    Completion of Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+            Console.WriteLine("What's your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guessNumber)
+                {
+                    Console.WriteLine("Guess higher");
+
+                }
+                else if (magicNumber < guessNumber)
+                {
+                    Console.WriteLine("Guess lower");
+                }
+                else 
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+        }
     }
+       
 }
