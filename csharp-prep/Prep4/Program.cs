ff5bd0964086b1d5d4f7dca53938a425eca41250
Xolani-Titus <tit19002@byui.edu>
2024-04-27 19:20:11 +0200

    Completion of Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int numberFromUser = -1;
+        while (numberFromUser != 0)
+        {
+            Console.WriteLine("Enter number (0 to quit): ");
+            string userReply = Console.ReadLine();
+            numberFromUser = int.Parse(userReply);
+
+            if (numberFromUser != 0)
+            {
+                numbers.Add(numberFromUser);
+            }
+        }
+
+        //Calculate the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"Sum: {sum}");
+
+        //Calculate average
+        float average = ((float)sum)/numbers.Count;
+        Console.WriteLine($"Average: {average}");
+
+        //Maximum number
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+                if (number > max)
+        {
+            max = number;
+        }
+        }
+        Console.WriteLine($"Maximum: {max}");
+        
+
+
+
+
+
+        
     }
 }
